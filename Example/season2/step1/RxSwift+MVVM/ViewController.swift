b367299e351c5e771e2a5180d891816ed0798dbd
yumin-jung <jungym887@naver.com>
2025-03-24 13:30:56 +0900

    RxSwift 정상적인 사용 방법
    - 단점: 너무 길고 귀찮음

--- Example/season2/step1/RxSwift+MVVM/ViewController.swift
+++ Example/season2/step1/RxSwift+MVVM/ViewController.swift
@@ -46,25 +46,9 @@ class ViewController: UIViewController {
     func downloadJson(_ url: String) -> Observable<String?> {
         // 1. 비동기로 생기는 데이터를 Observable로 감싸서 리턴하는 방법
         return Observable.create { emitter in
-            let url = URL(string: url)!
-            let task = URLSession.shared.dataTask(with: url) { data, _, err in
-                guard err == nil else {
-                    emitter.onError(err!)
-                    return
-                }
-                
-                if let dat = data, let json = String(data: dat, encoding: .utf8) {
-                    emitter.onNext(json)
-                }
-                
-                emitter.onCompleted()
-            }
-            
-            task.resume()
-            
-            return Disposables.create {
-                task.cancel()
-            }
+            emitter.onNext("Hello World")
+            emitter.onCompleted()
+            return Disposables.create()
         }
     }
 

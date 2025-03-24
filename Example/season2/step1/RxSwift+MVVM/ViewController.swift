386e5011432946c05bfd77a7a8a1dc2c9ba83902
yumin-jung <jungym887@naver.com>
2025-03-24 13:13:39 +0900

    RxSwift 기본적인 사용 방법

--- Example/season2/step1/RxSwift+MVVM/ViewController.swift
+++ Example/season2/step1/RxSwift+MVVM/ViewController.swift
@@ -6,34 +6,34 @@
 //  Copyright © 2019 iamchiwon. All rights reserved.
 //
 
-//import RxSwift
+import RxSwift
 import SwiftyJSON
 import UIKit
 
 let MEMBER_LIST_URL = "https://my.api.mockaroo.com/members_with_avatar.json?key=44ce18f0"
 
-class Observable<T> {
-    private let task: (@escaping (T) -> Void) -> Void
-    init(task: @escaping (@escaping (T) -> Void) -> Void) {
-        self.task = task
-    }
-    
-    func subscribe(_ f: @escaping (T) -> Void) {
-        task(f)
-    }
-}
+//class Observable<T> {
+//    private let task: (@escaping (T) -> Void) -> Void
+//    init(task: @escaping (@escaping (T) -> Void) -> Void) {
+//        self.task = task
+//    }
+//    
+//    func subscribe(_ f: @escaping (T) -> Void) {
+//        task(f)
+//    }
+//}
 
 class ViewController: UIViewController {
     @IBOutlet var timerLabel: UILabel!
     @IBOutlet var editView: UITextView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
             self?.timerLabel.text = "\(Date().timeIntervalSince1970)"
         }
     }
-
+    
     private func setVisibleWithAnimation(_ v: UIView?, _ s: Bool) {
         guard let v = v else { return }
         UIView.animate(withDuration: 0.3, animations: { [weak v] in
@@ -44,15 +44,26 @@ class ViewController: UIViewController {
     }
     
     func downloadJson(_ url: String) -> Observable<String?> {
-        return Observable() { f in
-            DispatchQueue.global().async {
-                let url = URL(string: url)!
-                let data = try! Data(contentsOf: url)
-                let json = String(data: data, encoding: .utf8)
-                
-                DispatchQueue.main.async {
-                    f(json)
+        // 1. 비동기로 생기는 데이터를 Observable로 감싸서 리턴하는 방법
+        return Observable.create { emitter in
+            let url = URL(string: url)!
+            let task = URLSession.shared.dataTask(with: url) { data, _, err in
+                guard err == nil else {
+                    emitter.onError(err!)
+                    return
                 }
+                
+                if let dat = data, let json = String(data: dat, encoding: .utf8) {
+                    emitter.onNext(json)
+                }
+                
+                emitter.onCompleted()
+            }
+            
+            task.resume()
+            
+            return Disposables.create {
+                task.cancel()
             }
         }
     }
@@ -65,10 +76,19 @@ class ViewController: UIViewController {
         editView.text = ""
         setVisibleWithAnimation(self.activityIndicator, true)
         
-        downloadJson(MEMBER_LIST_URL)
-            .subscribe { json in
-            self.editView.text = json
-            self.setVisibleWithAnimation(self.activityIndicator, false)
+        // 2. Observable로 오는 데이터를 받아서 처리하는 방법
+        _ = downloadJson(MEMBER_LIST_URL)
+            .subscribe { event in
+                switch event {
+                case .next:
+                    break
+                    
+                case .error:
+                    break
+                    
+                case .completed:
+                    break
+                }
         }
     }
 }
